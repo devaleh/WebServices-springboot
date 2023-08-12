5d58ec5a2b7224d7c60981e147aee2465c4526e4
Alexandre Assuncao <alexandre.myemail@gmail.com>
2023-08-12 19:08:47 -0300

    User insert

--- src/main/java/com/devaleh/webservices/services/UserService.java
+++ src/main/java/com/devaleh/webservices/services/UserService.java
@@ -3,7 +3,9 @@ package com.devaleh.webservices.services;
 import com.devaleh.webservices.entities.User;
 import com.devaleh.webservices.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,12 +16,16 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
 
-    public User findById(Long id){
+    public User findById(Long id) {
        Optional<User> obj = repository.findById(id);
        return obj.get();
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
     }
 }
